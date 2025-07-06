ff6546537aeae6114c8e768094785b94c83d7e67
AAlkazraji <ahmedaljro69@gmail.com>
2025-07-06 16:19:54 +1000

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/notfound" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
